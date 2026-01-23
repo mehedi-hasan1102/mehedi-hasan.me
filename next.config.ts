13374ec5899d14cd970c3271510b2c9eaaa34f5f
Mehedi Hasan <mehedi.hasan11023@gmail.com>
2026-01-23 23:04:55 +0600

    fix: next config ! change domain

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["mehedi-hasan.me", "m-hasan.vercel.app"],
+    domains: ["mehedi-hasan.me"],
   },
 };
 
